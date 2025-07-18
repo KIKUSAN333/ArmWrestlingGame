5cdaad130a9d4ebf03325831844e91403df9a515
Reon Kikuchi <t23cs015@yamanashi.ac.jp>
2025-07-18 09:25:17 +0900

    [clean]フォントをまとめました

--- src/HowToPlayState.java
+++ src/HowToPlayState.java
@@ -7,11 +7,17 @@ import java.awt.Toolkit;
 public class HowToPlayState implements State{
 	
 	private Image backgroundImage;
-	
+	private Font normalFont;
+	private Font titleFont;
+	private Font bigFont;
 	
 
 	public HowToPlayState() {
 		backgroundImage =  Toolkit.getDefaultToolkit().getImage(getClass().getResource("background.jpg"));
+		
+		titleFont = new Font("Arial", Font.BOLD, 32);
+		normalFont = new Font("Arial", Font.PLAIN, 16);
+		bigFont = new Font(Font.SANS_SERIF, Font.BOLD, 32);
 	}
 
 	@Override
@@ -36,17 +42,17 @@ public class HowToPlayState implements State{
 		
         // タイトル
         g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, 32));
+        g.setFont(titleFont);
         g.drawString("=== HOW TO PLAY ===", 100, 50);
         
 		
-       g.setFont(new Font("Arial", Font.PLAIN, 16));
+       g.setFont(normalFont);
 		g.drawString("ENTER : 決定", 0, 100);
 		g.drawString("上矢印キー : 上を選択", 0, 150);
 		g.drawString("下矢印キー : 下を選択", 0, 200);
 		g.drawString("SPACE : 戻る",0,250);
 		
-		g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 32));
+		g.setFont(bigFont);
 		g.drawString("ゲーム操作", 0, 350);
 		
         g.setFont(new Font("Arial", Font.PLAIN, 16));
